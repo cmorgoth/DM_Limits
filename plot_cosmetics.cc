6f2f0e77db7a7eb7c393aa2b9f3858a8f7d5c2b3
cmorgoth <cristian.morgoth@gmail.com>
2013-09-03 23:01:36 -0700

    Limits Plots Cosmetics

---
+++ plot_cosmetics.cc
@@ -0,0 +1,71 @@
+{
+
+  TFile* f = new TFile("graphs.root");
+
+  TGraph* Ld = (TGraph*)f->Get("g0_05_lambda");
+  TGraph* Lu = (TGraph*)f->Get("g1_05_lambda");
+  TGraph* Lmono = (TGraph*)f->Get("g_mono_lambda_SD");
+   
+  TCanvas* C = new TCanvas("C", "C", 640, 640);
+  C->cd();
+  TString labelX = "M_{#chi} (GeV)";
+  TString labelY = "#Lambda (GeV)";
+  
+  Ld->SetLineColor(kBlue);
+  Ld->SetLineWidth(2);
+  Ld->SetLineStyle(2);
+  
+  
+  Lu->SetLineColor(kRed);
+  Lu->SetLineWidth(2);
+  Lu->SetLineStyle(2);
+  Lmono->SetLineColor(kGreen);
+  Lmono->SetLineWidth(2);
+  Lmono->SetLineStyle(2);
+  
+  Ld->GetYaxis()->SetTitle(labelY);
+  Ld->GetXaxis()->SetTitleOffset(1.1);
+  Ld->GetXaxis()-> CenterTitle();
+  Ld->GetYaxis()->SetTitleOffset(1.1);
+  Ld->GetYaxis()-> CenterTitle();
+  
+
+  TMultiGraph *mg = new TMultiGraph();
+  mg->Add(Lmono);
+  mg->Add(Ld);
+  mg->Add(Lu);
+  mg->Draw("ALP");
+  mg->GetXaxis()->SetTitle(labelX);
+  mg->GetYaxis()->SetTitle(labelY);
+  mg->GetXaxis()->SetTitleOffset(1.1);
+  mg->GetXaxis()-> CenterTitle();
+  mg->GetYaxis()->SetTitleOffset(1.1);
+  mg->GetYaxis()-> CenterTitle();
+  mg->GetYaxis()->SetRangeUser(300, 1400);
+  
+  TLatex *t = new TLatex();
+  t->SetNDC();
+  t->SetTextAlign(22);
+  t->SetTextSize(0.03);
+  t->DrawLatex(0.25,0.87,"CMS Preliminary");
+  t->DrawLatex(0.25,0.83,"#sqrt{s} = 8 TeV");
+  
+  TLegend* leg = new TLegend(0.45, 0.75, 0.89, 0.89);//(xmin, ymin, xmax, ymax)
+  leg->AddEntry(Ld, "Razor A-V current u-quark (5 fb^{-1})" ,"l");
+  leg->AddEntry(Lu, "Razor A-V current d-quark (5 fb^{-1})" ,"l");
+  leg->AddEntry(Lmono, "Monojer combined limit (19.5 fb^{-1})" ,"l");
+  leg->SetFillColor(0);
+  leg->SetBorderSize(0);
+  leg->SetTextSize(0.03);
+  leg->SetTextSize(.022);
+  leg->SetFillColor(0);
+  leg->Draw();
+
+  C->SetLogy();
+  C->SetLogx();
+  C->Update();
+  C->SaveAs("AV_LambdaLimit.pdf");
+  C->SaveAs("AV_LambdaLimit.png");
+
+
+}
---
+++ plot_cosmetics_Sigma_SD.cc
@@ -0,0 +1,77 @@
+{
+
+  TFile* f = new TFile("graphs.root");
+
+  TGraph* Ld = (TGraph*)f->Get("g0_05_SD");
+  TGraph* Lu = (TGraph*)f->Get("g1_05_SD");
+  TGraph* Lmono = (TGraph*)f->Get("g_mono_SI");
+   
+  TCanvas* C = new TCanvas("C", "C", 640, 640);
+  C->cd();
+  TString labelX = "M_{#chi} (GeV)";
+  TString labelY = "#sigma_{N-#chi} (cm^{2})";
+  
+  TPad *pad1 = new TPad("pad1","pad1",.0,0.,1,1);
+  //pad1->SetBottomMargin(0);
+  pad1->SetLeftMargin(0.12);
+  pad1->Draw();
+  pad1->cd();
+
+  Ld->SetLineColor(kBlue);
+  Ld->SetLineWidth(2);
+  Ld->SetLineStyle(2);
+  
+  
+  Lu->SetLineColor(kRed);
+  Lu->SetLineWidth(2);
+  Lu->SetLineStyle(2);
+  Lmono->SetLineColor(kGreen);
+  Lmono->SetLineWidth(2);
+  Lmono->SetLineStyle(2);
+  
+  Ld->GetYaxis()->SetTitle(labelY);
+  Ld->GetXaxis()->SetTitleOffset(1.1);
+  Ld->GetXaxis()-> CenterTitle();
+  Ld->GetYaxis()->SetTitleOffset(1.3);
+  Ld->GetYaxis()-> CenterTitle();
+  
+
+  TMultiGraph *mg = new TMultiGraph();
+  mg->Add(Lmono);
+  mg->Add(Ld);
+  mg->Add(Lu);
+  mg->Draw("ALP");
+  mg->GetXaxis()->SetTitle(labelX);
+  mg->GetYaxis()->SetTitle(labelY);
+  mg->GetXaxis()->SetTitleOffset(1.1);
+  mg->GetXaxis()-> CenterTitle();
+  mg->GetYaxis()->SetTitleOffset(1.7);
+  mg->GetYaxis()-> CenterTitle();
+  mg->GetYaxis()->SetRangeUser(1e-41, 8e-37);
+  
+  TLatex *t = new TLatex();
+  t->SetNDC();
+  t->SetTextAlign(22);
+  t->SetTextSize(0.03);
+  t->DrawLatex(0.25,0.87,"CMS Preliminary");
+  t->DrawLatex(0.25,0.83,"#sqrt{s} = 8 TeV");
+  
+  TLegend* leg = new TLegend(0.4, 0.75, 0.89, 0.89);//(xmin, ymin, xmax, ymax)
+  leg->AddEntry(Ld, "Razor SD u-quark (5 fb^{-1})" ,"l");
+  leg->AddEntry(Lu, "Razor SD d-quark (5 fb^{-1})" ,"l");
+  leg->AddEntry(Lmono, "Monojer SD combined limit (19.5 fb^{-1})" ,"l");
+  leg->SetFillColor(0);
+  leg->SetBorderSize(0);
+  leg->SetTextSize(0.03);
+  leg->SetTextSize(.022);
+  leg->SetFillColor(0);
+  leg->Draw();
+
+  pad1->SetLogy();
+  pad1->SetLogx();
+  pad1->Update();
+  C->SaveAs("SD_SigmaLimit.pdf");
+  C->SaveAs("SD_SigmaLimit.png");
+
+
+}
